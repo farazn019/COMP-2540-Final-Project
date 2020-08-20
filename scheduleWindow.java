19b933efe92f6085e5c0ba10b4d5cb335f3823a3
FarazNaseem <faraz.n019@outlook.com>
2020-08-20 01:49:05 -0400

    Fixed the buttons clustering together, and added a label to the home page.

--- scheduleWindow.java
+++ scheduleWindow.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Dimension;
+import javax.swing.WindowConstants;
 import javax.swing.*;
+import javax.swing.JLabel;
 import java.lang.*;
 
 public class scheduleWindow extends JFrame{
@@ -8,23 +11,27 @@ public class scheduleWindow extends JFrame{
     private String windowName;
     private int xValue, yValue, width = 0, height = 0;
     private double totalSum;
-
-    JFrame defaultFrame;
     JPanel panel = new JPanel(new GridBagLayout());
+    JFrame defaultFrame;
     GridBagConstraints constraints = new GridBagConstraints();
 
     void addWindow(int widthOfTemplate, int heightOfTemplate){
-        width = widthOfTemplate;
-        height = heightOfTemplate;
-        JFrame  frame = new JFrame("Invoice System");
-        frame.setPreferredSize(new Dimension(widthOfTemplate, heightOfTemplate));
-        frame.setResizable(false);
-        frame.setVisible(true);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        defaultFrame = frame;
-        defaultFrame.getContentPane().add(panel);
+        this.width = widthOfTemplate;
+        this.height = heightOfTemplate;
 
+        JFrame  frame = new JFrame("Invoice System");
+        frame.getContentPane();
+
+        JLabel title = new JLabel("Click on one of the buttons to view your schedule: ");
+        Dimension size = title.getPreferredSize();
+        title.setBounds(this.width / 3, 100, size.width, size.height);
+        panel.setLayout(null);
+        panel.add(title);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setSize(1000, 600);
+        frame.setVisible(true);
+        defaultFrame = frame;
     }
 
     void setWindowName(String windowName){
@@ -40,8 +47,8 @@ public class scheduleWindow extends JFrame{
     void addDay(){
         JButton dayButton[] = new JButton[7];
         int current_x_position = 0;
-        int current_y_position = 0;
-        panel.setLayout(new GridBagLayout());
+        final int current_y_position = 350;
+
         for(int current_index = 0; current_index < days.length; current_index++){
             dayButton[current_index] = new JButton(days[current_index]);
             dayButton[current_index].addActionListener(new ActionListener() {
@@ -54,13 +61,10 @@ public class scheduleWindow extends JFrame{
                     new_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 }
             });
-            constraints.fill = GridBagConstraints.HORIZONTAL;
-            constraints.weighty = 1;
-            constraints.gridx = current_x_position;
-            constraints.gridy = current_y_position;
-            constraints.gridheight = 1;
-            current_y_position += 1;
+            Dimension buttonSize = dayButton[current_index].getPreferredSize();
+            dayButton[current_index].setBounds(current_x_position, current_y_position, 120, buttonSize.height);
             panel.add(dayButton[current_index]);
+            current_x_position += 125;
         }
 
 
